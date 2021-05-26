ee1d8f0fc6dc843127f29fefd5e18f2efa55a330
Call-Me-AJ-01 <84077372+Call-Me-AJ-01@users.noreply.github.com>
2021-05-26 05:46:10 +0530

    Add files via upload

---
+++ (11) BellmanFord Algorithm/Bellman Ford Algorithm.py
@@ -0,0 +1,100 @@
+class Edge:
+def __init__(self, weight, start_node, end_node):
+        self.start_node = start_node
+        self.end_node = end_node
+        self.weight = weight
+
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.previous_node = None
+        self.adjacency_list = []
+        self.min_distance = float('inf')
+
+
+class Bellmanford:
+    def __init__(self, vertex_list, edge_list, start_vertex):
+        self.vertex_list = vertex_list
+        self.edge_list = edge_list
+        self.start_vertex = start_vertex
+        self.cycle = False
+
+    def find_shortest_path(self):
+        self.start_vertex.min_distance = 0
+        for vertex_iteration in range(len(self.vertex_list) - 1):
+            for each_edge in self.edge_list:
+                if each_edge.start_node.min_distance + each_edge.weight < each_edge.end_node.min_distance:
+                    each_edge.end_node.previous_node = each_edge.start_node
+                    each_edge.end_node.min_distance = each_edge.start_node.min_distance + each_edge.weight
+
+        for edge in self.edge_list:
+            if self.check_cycle(edge) is True:
+                print('Negative Cycle Detected')
+                return
+
+    def check_cycle(self, edge):
+        if edge.start_node.min_distance + edge.weight < edge.end_node.min_distance:
+            self.cycle = True
+            return True
+        else:
+            return False
+
+    def get_shortest_path(self, vertex):
+
+        if self.cycle is not True:
+            print("Shortest path exists with value: ", vertex.min_distance)
+            node = vertex
+
+            while node is not None:
+                print(node.name)
+                node = node.previous_node
+        else:
+            print('There is a negative cycle in the G(V,E) graph...')
+
+
+if __name__ == '__main__':
+    # we have to create the vertices
+    node1 = Node("A")
+    node2 = Node("B")
+    node3 = Node("C")
+    node4 = Node("D")
+    node5 = Node("E")
+    node6 = Node("F")
+    node7 = Node("G")
+
+    # let's create the edges
+    edge1 = Edge(5, node1, node2)
+    edge2 = Edge(9, node1, node5)
+    edge3 = Edge(4, node2, node5)
+    edge4 = Edge(12, node2, node3)
+    edge5 = Edge(7, node2, node4)
+    edge6 = Edge(3, node3, node4)
+    edge7 = Edge(1, node3, node6)
+    edge8 = Edge(9, node4, node7)
+    edge9 = Edge(6, node5, node3)
+    edge10 = Edge(4, node5, node6)
+    edge11 = Edge(2, node6, node7)
+    edge12 = Edge(-6, node7, node3)
+
+    # handle the adjacency lists
+    node1.adjacency_list.append(edge1)
+    node1.adjacency_list.append(edge2)
+    node2.adjacency_list.append(edge3)
+    node2.adjacency_list.append(edge4)
+    node2.adjacency_list.append(edge5)
+    node3.adjacency_list.append(edge6)
+    node3.adjacency_list.append(edge7)
+    node4.adjacency_list.append(edge8)
+    node5.adjacency_list.append(edge9)
+    node5.adjacency_list.append(edge10)
+    node6.adjacency_list.append(edge11)
+    node7.adjacency_list.append(edge12)
+
+    # run the algorithm
+    vertices = (node1, node2, node3, node4, node5, node6, node7)
+    edges = (edge1, edge2, edge3, edge4, edge5, edge6, edge7, edge8, edge9, edge10, edge11, edge12)
+
+    algorithm = Bellmanford(vertices, edges, node1)
+    algorithm.find_shortest_path()
+    algorithm.get_shortest_path(node7)
